596cc5449cd59a4b5d45b5f97bd4e6aa4520a0e9
Brijesh Kumar <brijes08@gmail.com>
2024-01-04 12:58:26 +0530

    dfgh

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,32 +3,27 @@ const User = require('../models/userSchema')
 
 const authenticate = async (req, res, next) => {
     try {
-        // console.log(req, '00')
-        const token = req.cookies.jwtoken
-        console.log(token, '0')
+        const token = req.cookies.jwtoken;
+        console.log(token, '0');
+
         const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
-        const rootUser = await User.findOne({_id:verifyToken._id, "tokens.token": token})
+        const rootUser = await User.findOne({_id: verifyToken._id, "tokens.token": token});
 
         req.user = verifyToken.user;
+        console.log(rootUser, '1');
 
-        console.log(rootUser, '1')
-        if(!rootUser){
-            throw new Error('User Not Found')
+        if (!rootUser) {
+            throw new Error('User Not Found');
         } else {
-            req.token = token
-            req.rootUser = rootUser
-            req.userID = rootUser._id
-            next()
-            console.log(req.rootUser, '2')
+            req.token = token;
+            req.rootUser = rootUser;
+            req.userID = rootUser._id;
+            console.log(req.rootUser, '2');
+            next();
         }
-        
-        next();
     } catch (err) {
-        // res.status(400).send('Unauthorized:No token provided');
-        
         console.error('JWT Verification Error:', err.message);
         return res.status(401).json({ message: 'Unauthorized' });
-        // console.log(err)
     }
 }
 
