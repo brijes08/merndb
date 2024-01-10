5b6a0beacc6750e782c68d28f0fb5193e0dea121
Brijesh Kumar <brijes08@gmail.com>
2024-01-10 17:32:11 +0530

    ss

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const cors = require("cors")
 const dotenv = require("dotenv")
@@ -10,7 +11,23 @@ const app = express();
 dotenv.config({ path: "./config.env" })
 require("./DB/connection")
 
-app.use(express.static(path.join(__dirname, 'public/')));
+
+if (mongoose.connection.readyState === 0) {
+    mongoose.connect('mongodb+srv://brijes08:X5OJqrp6Vzu4H3hm@cluster0.m1ht25f.mongodb.net/mernstack', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+// Create a mongoose model for your images
+const Image = mongoose.model('Image', {
+  filename: String, // Assuming your images have a filename field in the database
+  data: Buffer,     // Assuming your images are stored as Buffer data in the database
+});
+
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -25,7 +42,27 @@ app.all("*", (req, res, next) => {
 next();
 });
 
-
+app.get('/images/:filename', async (req, res) => {
+    try {
+      const filename = req.params.filename;
+  
+      // Find the image in the database by filename
+      const image = await Image.findOne({ filename });
+  
+      if (!image) {
+        return res.status(404).send('Image not found');
+      }
+  
+      // Set the appropriate content type
+      res.contentType('image/png'); // Adjust content type based on your image format
+  
+      // Send the image data
+      res.send(image.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 
  
 const PORT = process.env.PORT;
