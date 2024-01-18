44ae80dba6dff8736ded8918d3f53c9c4eaebf75
Brijesh Kumar <brijes08@gmail.com>
2024-01-18 14:07:50 +0530

    fds

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const Image = mongoose.model('Image', {
 
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "50mb" }));
